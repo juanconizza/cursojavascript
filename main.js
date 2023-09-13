137d9844e0b5f5ccf2db849f215a3ffd8d552af1
juanconizza <juanconizza@gmail.com>
2023-09-13 15:52:38 -0300

    Clase1

---
+++ main.js
@@ -0,0 +1,60 @@
+// ASI SE COMENTA EN UNA SOLA LINEA
+
+
+/* COMENTARIO DE BLOQUE
+
+PARA COMENTARIO MAS 
+
+AMPLIO */
+
+
+//DECLARAMOS UNA VARIABLE. CON PUNTO Y COMA AL FINAL
+//let edad = 5;
+
+// SIMPRE QUE LLAMAEMOS LA VARIABLE HARA REFERENCIA AL 6
+
+//let nombre = "andres"; //string 
+
+//OTRA FORMA DE DECLARAR PERO NO SE USA MUCHO ES VAR
+
+//var direccion = "AV" //SE USA LET
+
+
+// Las variables deben ser nombres descriptivos con Camel Case 
+
+//let nombreDelParticipante = "Juan"
+
+//let apellidoDelParticipante = "Garcia"
+
+//let numeroUno = 3;
+//let numeroDos = 5;
+
+//et resultadosSuma = numeroUno + numeroDos;
+
+//console.log(resultadosSuma);
+
+
+//let nombre = "Juan";
+//let saludo = "Hola";
+
+//let saludoCompleto = saludo + " " + nombre;
+
+//CONSOLE.LOG se usa para visualizar con el code runner cada variable
+//console.log(saludoCompleto);
+
+//prompt, alert
+
+//prompt("Ingrese su nombre");
+
+/*let nombre = prompt("Ingrese su nombre");
+
+console.log(nombre);*/
+
+let numeroUno = Number(prompt("Ingrese el numero 1"));
+let numeroDos = Number(prompt("Ingrese el numero 2"));
+
+alert(numeroUno + numeroDos);
+
+
+
+
