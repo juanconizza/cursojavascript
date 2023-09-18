34ba8b1c282e3390182b84500efb86d07ffbf453
juanconizza <juanconizza@gmail.com>
2023-09-18 10:58:12 -0300

    Clase 2

---
+++ clase-2/main.js
@@ -0,0 +1,46 @@
+/*
+= asignacion
+== comparacion solo de valor
+=== comparacion de valor y tipo de dato
+*/
+
+/*let valor = true; //variable con tipo de dato booleano. Cuando es True o False
+let valor = edad > 100;*/
+
+/*estructura del condicional if
+
+if (condicion) {
+    codigo a ejecutar siempre que sea verdadera la condicion
+}
+*/
+
+/*let edad = 20;
+
+if(edad >=18){
+    console.log("Eres mayor de edad")
+};*/
+
+/*
+estructura de la condicion if else
+
+if(condicion){
+    codigo a ejecutar si es verdadera
+} else {
+    codigo a ejecutar si es falsa
+}
+*/
+
+/*let edad = parseInt(prompt("Ingrese edad"));
+
+if(edad >= 18){
+    alert("Eres mayor de edad")
+    }else( )*/
+
+   /* console.log("Hola")*/
+
+/*let nombre = "andres";
+//esta es la que vamos a usar si o si para concatenar
+console.log(`Hola ${nombre}`);*/
+
+
+
